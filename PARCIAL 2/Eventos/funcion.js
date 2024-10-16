e8f79cc701e9815e211c39dd1b9380a0cfadea6c
Adai <adai03db@gmail.com>
2024-10-16 10:59:32 -0600

    commit

---
+++ PARCIAL 2/Eventos/funcion.js
@@ -0,0 +1,25 @@
+Declaracion
+function calcularArea(base, altura) {
+    return (base * altura) / 2;
+}
+console.log(calcularArea(10, 5)); 
+
+Expresion
+const saludar = function(nombre) {
+    return Hola, ${nombre}!;
+};
+console.log(saludar("Carlos")); 
+
+const suma = 1+1
+console.log(suma);
+
+iffe
+(function() {
+    const mensaje = "Función autoejecutada";
+    console.log(mensaje);
+})();
+
+
+Flecha
+const area = x => x/2;
+console.log(area(10));
