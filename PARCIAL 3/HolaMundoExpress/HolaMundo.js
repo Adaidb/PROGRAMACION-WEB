e23c2f70f206aec01a81b5952b3dd24ec4451f98
Adai <adai03db@gmail.com>
2024-11-08 10:58:30 -0600

    Peticiones Express

--- PARCIAL 3/HolaMundoExpress/HolaMundo.js
+++ PARCIAL 3/HolaMundoExpress/HolaMundo.js
@@ -1,11 +1,29 @@
-const express= require ('express');
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.get("/", (req, res) => {
+  res.send("Hola Mundo!");
+});
+
+app.get('/administrativos', (req, res) => {
+  console.log(req.query);
+  res.send('Servidor contestando a la petición');
+});
+
+app.get('/maestros', (req, res) => {
+  console.log(req.body); 
+  res.send('Servidor contestando a la petición');
+});
+
+app.get('/estudiantes/:carrera', (req, res) => {
+  console.log(req.params.carrera);
+  console.log(req.query.control);
+  res.send('Servidor contestando a la petición');
+});
+
+app.listen(3000, () => {
+  console.log(`Example app listening on port 3000`);
+});
